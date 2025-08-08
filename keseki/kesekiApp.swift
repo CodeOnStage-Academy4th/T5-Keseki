2d2b3aa9e8b9bee4b620a0ee074263f21935d640
nell <devstory.co.kr@gmail.com>
2025-08-08 22:06:48 +0900

    Initial Commit

---
+++ keseki/kesekiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kesekiApp.swift
+//  keseki
+//
+//  Created by Nell on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct kesekiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
